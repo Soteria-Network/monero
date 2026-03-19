d270b617277b290bfa64faf5b6a9731adf532ebc
Soteria Network <166382245+Soteria-Network@users.noreply.github.com>
2026-03-19 07:35:54 +0300

    improve orphans

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -75,7 +75,7 @@
 #define DYNAMIC_FEE_PER_KB_BASE_FEE_V5                  ((uint64_t)2000000000 * (uint64_t)CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2 / CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V5)
 #define DYNAMIC_FEE_REFERENCE_TRANSACTION_WEIGHT         ((uint64_t)3000)
 
-#define ORPHANED_BLOCKS_MAX_COUNT                       100
+#define ORPHANED_BLOCKS_MAX_COUNT                       3000
 
 
 #define DIFFICULTY_TARGET_V2                            15  // seconds
